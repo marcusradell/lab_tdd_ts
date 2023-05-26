c4d2a6aa8505d94d1d9fda399b5ed36afa780bbb
Marcus Rådell <marcus@radell.net>
2023-05-26 16:35:39 +0200

    Logging in with email 'a@b.c' succeeds with a string representing the access token

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,10 @@ async function login(email: string) {
     return { status: "FAILED", code: "EMPTY_EMAIL" };
   }
 
+  if (email === "a@b.c") {
+    return { status: "SUCCEEDED", jwt: "not_a_good_secret" };
+  }
+
   return { status: "FAILED", code: "NOT_AN_EMAIL" };
 }
 
@@ -20,4 +24,12 @@ test("Logging in with email 'a@b' fails with validation error code 'NOT_AN_EMAIL
   const result = await login(email);
 
   expect(result).toEqual({ status: "FAILED", code: "NOT_AN_EMAIL" });
+});
+
+test("Logging in with email 'a@b.c' succeeds with a string representing the access token", async () => {
+  const email = "a@b.c";
+
+  const result = await login(email);
+
+  expect(result).toEqual({ status: "SUCCEEDED", jwt: "not_a_good_secret" });
 });

f0d13c8de4d00366068f16d3ce3a7cc00a4a832f
Marcus Rådell <marcus@radell.net>
2023-05-26 16:20:58 +0200

    Logging in with email 'a' fails with validation error code 'NOT_AN_EMAIL'

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,8 @@
 async function login(email: string) {
+  if (email === "a") {
+    return { status: "FAILED", code: "NOT_AN_EMAIL" };
+  }
+
   return { status: "FAILED", code: "EMPTY_EMAIL" };
 }
 
@@ -8,4 +12,12 @@ test("Logging in with empty email fails with validation error code 'EMPTY_EMAIL'
   const result = await login(email);
 
   expect(result).toEqual({ status: "FAILED", code: "EMPTY_EMAIL" });
+});
+
+test("Logging in with email 'a' fails with validation error code 'NOT_AN_EMAIL'", async () => {
+  const email = "a";
+
+  const result = await login(email);
+
+  expect(result).toEqual({ status: "FAILED", code: "NOT_AN_EMAIL" });
 });

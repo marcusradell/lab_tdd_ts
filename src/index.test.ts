806344e1e197e768bbf56e7538d03678a1ea695a
Marcus Rådell <marcus@radell.net>
2023-05-26 16:05:18 +0200

    Logging in with empty email fails with validation error code 'EMPTY_EMAIL'

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,11 @@
-test("it works!", () => {
-  expect(1).toEqual(1);
+async function login(email: string) {
+  return { code: "EMPTY_EMAIL" };
+}
+
+test("Logging in with empty email fails with validation error code 'EMPTY_EMAIL'", async () => {
+  const email = "";
+
+  const result = await login(email);
+
+  expect(result).toEqual({ code: "EMPTY_EMAIL" });
 });

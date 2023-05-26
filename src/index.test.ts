20debdb040c8e583a011713ed365988e12b27dce
Marcus Rådell <marcus@radell.net>
2023-05-26 16:25:59 +0200

    change test email to be a@b

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,8 +14,8 @@ test("Logging in with empty email fails with validation error code 'EMPTY_EMAIL'
   expect(result).toEqual({ status: "FAILED", code: "EMPTY_EMAIL" });
 });
 
-test("Logging in with email 'a' fails with validation error code 'NOT_AN_EMAIL'", async () => {
-  const email = "a";
+test("Logging in with email 'a@b' fails with validation error code 'NOT_AN_EMAIL'", async () => {
+  const email = "a@b";
 
   const result = await login(email);
 
